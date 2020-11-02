c30be782d62412ee8e9ea5766fbc91fe1975fcc1
Marc Kohlbau <m.kohlbau@myopenfactory.com>
2020-11-02 11:05:30 +0100

    Fix gradle git hook problem with '.'
    Signed-off-by: Marc Kohlbau <m.kohlbau@myopenfactory.com>

--- bash/git.sh
+++ bash/git.sh
@@ -1,6 +1,6 @@
 function git_gradle_hook {
     echo -n "Running gradle build: "
-    GRADLE_OUTPUT=$( ${1:.}/gradlew --console=plain -q build 2>&1 >/dev/null )
+    GRADLE_OUTPUT=$( ${1:-"."}/gradlew --console=plain -q build 2>&1 >/dev/null )
     if [ $? -ne 0 ]; then
         echo "❌"
         echo "$GRADLE_OUTPUT"
